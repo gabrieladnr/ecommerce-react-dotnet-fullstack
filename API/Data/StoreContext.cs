9bb56c608a793f6855a7bd5cf21262c7b2446587
Gabriela Ruas <gabriela.ruas.base2@unimedbh.com.br>
2025-01-22 19:29:08 -0300

    222. Creating a class to seed data into the database

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,16 +1,21 @@
-using API.Entities;
-using Microsoft.EntityFrameworkCore;
+using API.Entities; 
+using Microsoft.EntityFrameworkCore; 
 
 namespace API.Data
 {
+    // StoreContext is the bridge between code and database.
+    // It manages the connection, mapping of entities to database tables, and operations like querying or saving data.
     public class StoreContext : DbContext
     {
+        // Constructor that passes options (e.g., connection string, database provider) to the base DbContext.
+        // DbContextOptions are configured in the application startup, typically in Program.cs or Startup.cs.
         public StoreContext(DbContextOptions options) : base(options)
         {
-            
         }
 
-        public DbSet<Product> Products { get; set;}
-        
+        // DbSet represents a table in the database.
+        // Each DbSet property allows you to query and save instances of the corresponding entity type (Product in this case).
+        public DbSet<Product> Products { get; set; }
+
     }
 }
--- API/Entities/Product.cs
+++ API/Entities/Product.cs
@@ -6,6 +6,8 @@ namespace API.Entities
 
         public string Name { get; set; }
 
+        public string Description { get; set; }
+
         // could be decimal, but its long for the payment provider that will be used
         // long does not use decimal points, so will be stored in db $10000 and treated to show $100.00 
         public long Price { get; set; }
