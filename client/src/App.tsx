a4d460093fd5ce21fc947010f2b1b2f87f4c4b52
Gabriela Ruas <gabriela.ruas.base2@unimedbh.com.br>
2025-01-26 17:03:29 -0300

    23. React component basics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
+import { Fragment } from "react/jsx-runtime"
+const products = [
+  { name:'product1', price:'100.00'},
+  { name:'product2', price:'200.00'}
+]
+
 function App() {
 
   return (
-    <div style={{fontSize: '1.2=6rem'}}>ReStore</div>
+    <Fragment>
+      <h1 style={{color: "blueviolet"}}>Re-store</h1>
+      <ul>
+        {products.map((item, index) => ( // () implicit return, {} explicit return
+          <li key={index}>{item.name} - {item.price}</li>
+        ))}
+      </ul>
+    </Fragment>
   )
 }
 
