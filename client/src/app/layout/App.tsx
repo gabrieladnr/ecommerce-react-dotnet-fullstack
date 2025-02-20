4ac2e9d57f8f11896812bfc8e0044bbfc634e836
Gabriela Ruas <gabriela.ruas.base2@unimedbh.com.br>
2025-02-20 07:59:33 -0300

    fix: unused imports

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,7 @@
 import { Fragment, useEffect, useState } from "react";
 import { Product } from "../models/product";
 import Catalog from "../../feature/catalog/Catalog";
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Container } from "@mui/material";
 import NavBar from "../../feature/catalog/NavBar";
 
 function App() {
